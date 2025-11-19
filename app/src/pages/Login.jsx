dffa275685789a3f846d6f67b3dca7c0bf6d03aa
Eduardosntr <alu0101565590@ull.edu.es>
2025-11-19 13:51:05 +0000

    Fix en el login

--- app/src/pages/Login.jsx
+++ app/src/pages/Login.jsx
@@ -132,7 +132,7 @@ const Login = ({ setUser }) => {
               {/* Email Input */}
               <div>
                 <label htmlFor="email" className="block text-sm font-semibold text-gray-700 mb-2">
-                  Usuario
+                  Email
                 </label>
                 <div className="relative">
                   <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
