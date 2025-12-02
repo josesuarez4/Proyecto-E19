e7ad533759115c0681e04636f634caef7e72be4b
josesuarez4 <alu0101556376@ull.edu.es>
2025-12-02 23:46:50 +0000

    añadir codecov

--- server/__tests__/routes/usuarios.test.js
+++ server/__tests__/routes/usuarios.test.js
@@ -233,6 +233,7 @@ describe('Usuarios Routes', () => {
       expect(response.body.password).toBeUndefined();
     });
 
+    
     it('debería actualizar estado activo del usuario', async () => {
       const user = await User.create({
         name: 'User',
